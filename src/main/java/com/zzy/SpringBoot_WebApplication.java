9b3b69f18efe79535d59cef9b9878e337184a397
zhangzhonghua <583705252@163.com>
2020-08-04 19:19:26 +0800

    v1.6

--- src/main/java/com/zzy/SpringBoot_WebApplication.java
+++ src/main/java/com/zzy/SpringBoot_WebApplication.java
@@ -1,13 +1,14 @@
 package com.zzy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.zzy.dao")
 public class SpringBoot_WebApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBoot_WebApplication.class,
-                args);
+        SpringApplication.run(SpringBoot_WebApplication.class,args);
     }
     }
 
--- src/main/java/com/zzy/controller/UserController.java
+++ src/main/java/com/zzy/controller/UserController.java
@@ -1,4 +1,28 @@
 package com.zzy.controller;
 
+import com.zzy.entity.User;
+import com.zzy.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
+    @GetMapping("/find/{id}")
+    public String findUserById(Model model,@PathVariable("id") Integer id){
+        System.out.println("进入handler");
+        User user = userService.findById(id);
+        model.addAttribute("user",user);
+        System.out.println("跳转页面");
+        return "first";
+    }
+    @GetMapping("/delete")
+    public String deleteUser(Integer id){
+        System.out.println("根据id删除");
+        return "redirect:/login.html";
+    }
 }
